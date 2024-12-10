80d2b1270566f9ed2a3858b240933b8d06f40386
Alan José <alanjsdelima@gmail.com>
2024-12-10 10:28:30 -0300

    Adding CLI params parser

--- main.cc
+++ main.cc
@@ -1,14 +1,41 @@
 #include "Headers/RandomWalkModel.h"
 #include "Headers/State.h"
+#include <unistd.h>
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    //Default params.
     int numberOfRuns = 1000;
     int gridSize = 100;
-    int numberOfGenerations = 52;
-    bool applySocialDistanceEffect = true;
+    int numberOfGenerations = 10;
+    bool applySocialDistanceEffect = false;
     
+    //Don't move.
+    int cliOption;
+    while((cliOption = getopt(argc, argv, "r:g:n:s:")) != -1) {
+        switch(cliOption) {
+            case 'r':
+                numberOfRuns = stoi(optarg);
+                break;
+            case 'g':
+                gridSize = stoi(optarg);
+                break;
+            case 'n':
+                numberOfGenerations = stoi(optarg);
+                break;
+            case 's':
+                applySocialDistanceEffect = stoi(optarg) != 0;
+                break;
+            default:
+                cerr << "Usage: " << argv[0] << 
+                " [-r numberOfRuns] [-g gridSize] [-n numberOfGenerations] [-s applySocialDistanceEffect (1/0)]"
+                << endl;
+                return EXIT_FAILURE;
+        }
+    }
+
+    //Ok, you can change here.
     vector<vector<double>> transitionProbabilities = {
         {0.62, 0.3, 0.05, 0.0, 0.03}, // healthy
         {0.05, 0.64, 0.1, 0.01, 0.2}, // isolated
@@ -20,6 +47,7 @@ int main() {
     for(int i = 0; i < numberOfRuns; ++i) {
         RandomWalkModel model(gridSize, applySocialDistanceEffect);
         model.simulation(numberOfGenerations);
+        //Print the individuals count based on current state.
         cout << model.getStateCount(State::dead) << endl;
     }
 
